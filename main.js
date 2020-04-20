17c1306acf05c7da5486665c92ab82f315fd84a8
Archana Karki <39858235+archanakarki@users.noreply.github.com>
2020-04-20 23:58:57 +0300

    Made display working for individual button clicked

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 let buttons = document.querySelectorAll('button');
+let displayValues = document.querySelector('#display_values');
 
-for(let button of buttons){
-    button.addEventListener('click', ()=>{
-        let value = button.value;
+for (let button of buttons) {
+    button.addEventListener('click', (e) => {
+        let value = e.target.value;
         console.log(`The value is ` + value)
+        displayValues.textContent = value;
     })
 }
+
+//
