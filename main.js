d69ad10152f3c60aa2f1684008a92550cb623715
Archana Karki <39858235+archanakarki@users.noreply.github.com>
2020-04-21 02:13:35 +0300

    Adjusting display value size

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 let buttons = document.querySelectorAll('button');
 let displayValues = document.querySelector('#display_values');
 
+
+const concatenateString = (val) => {
+    let string = "";
+    string += val;
+    return string;
+}
+
+let string = "";
 for (let button of buttons) {
     button.addEventListener('click', (e) => {
-        let value = e.target.value;
-        console.log(`The value is ` + value)
-        displayValues.textContent = value;
+        let value = "" + e.target.value;
+        string = string + value;
+        displayValues.textContent = string;
+
+        if(displayValues.textContent.length > 10 && displayValues.textContent.length < 22){
+            displayValues.style.fontSize = "4.3rem";
+            displayValues.style.lineHeight = "1";
+        } else if (displayValues.textContent.length > 22) {
+            displayValues.style.fontSize = "3.8rem";
+            displayValues.style.lineHeight = "1";
+        } else if (displayValues.textContent.length > 26 && displayValues.textContent.length < 39) {
+            displayValues.style.fontSize = "2.5rem";
+            displayValues.style.lineHeight = "1";
+        }
+        console.log(`The value of string is ` + string)
     })
 }
 
