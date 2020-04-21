142b8bf77cace3fba981028145f398149f7a6d07
Archana Karki <39858235+archanakarki@users.noreply.github.com>
2020-04-21 23:55:19 +0300

    Making calculator an object

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-let buttons = document.querySelectorAll('button');
-let displayValues = document.querySelector('#display_values');
+let buttons = document.getElementsByTagName('button');
+let display = document.getElementById('display');
+let displayValues = document.getElementById('display_values');
 
-
-const concatenateString = (val) => {
-    let string = "";
-    string += val;
-    return string;
-}
 
 let string = "";
+let operandAndOperator = [];
 for (let button of buttons) {
     button.addEventListener('click', (e) => {
-        let value = "" + e.target.value;
+        let value = `${e.target.value}`;
+        // addToArray(e.target.value);
         string = string + value;
         displayValues.textContent = string;
-
-        if (displayValues.textContent.length > 10 && displayValues.textContent.length < 22) {
-            displayValues.style.fontSize = "4.3rem";
-            displayValues.style.lineHeight = "1";
-        } else if (displayValues.textContent.length > 22) {
-            displayValues.style.fontSize = "3.8rem";
-            displayValues.style.lineHeight = "1";
-        } else if (displayValues.textContent.length > 26 && displayValues.textContent.length < 39) {
-            displayValues.style.fontSize = "2.5rem";
-            displayValues.style.lineHeight = "1";
-        }
-        console.log(`The value of string is ` + string)
+        console.log(displayValues.textContent)
     })
 }
 
-//
+
+const calculator = {
+    valuesToDisplay: string
+    // operators: [],
+    // sum: add(),
+    // difference: subtract(),
+    // product: multiply(),
+    // division: divide()
+}
+
+const addToArray = (value) => {
+    // if(typeof(val) == ){
+
+    // }
+}
