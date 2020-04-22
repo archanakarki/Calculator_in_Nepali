b8f8c0db29aef1222a1bc473ce0065bd5daf760e
Archana Karki <39858235+archanakarki@users.noreply.github.com>
2020-04-22 13:59:28 +0300

    String concatenates and ends when + operator is clicked

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
+"use strict";
+
 let buttons = document.getElementsByTagName('button');
 let display = document.getElementById('display');
 let displayValues = document.getElementById('display_values');
 
-
-// let string = "";
+let string = "";
+let arrOfInputs = [];
 for (let button of buttons) {
     button.addEventListener('click', (e) => {
-        let value = `${e.target.value}`;
+
+        /* If = is clicked -> functions */
+        /* If numbers clicked -> add to array */
+        let operatorCheckerRegex = /[+]/g;
+        let numberCheckerRegex = /[0-9०-९.]/ig;
+        let value = e.target.value;
         // string = string + value;
-        // displayValues.textContent = string; //Directly making string might not be the efficient way
-    })
+
+        //Multiple statement ternery operator in use
+        numberCheckerRegex.test(value) ? string = string + value : (operatorCheckerRegex.test(value) ? (arrOfInputs.push.apply(arrOfInputs, [string, value])) : console.log("Not a num and +"));
+        console.log(string, arrOfInputs)
+    })  
+}  
+
+let calculate = (str) => {
+
 }
